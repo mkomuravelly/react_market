d4443266c501dd6208cd1469c23fe8b2ad2e8c1f
Madhuri <madhuri2692@gmail.com>
2019-11-19 23:12:53 -0600

    initial commit

--- src/home/home.jsx
+++ src/home/home.jsx
@@ -1,6 +1,6 @@
-import React,{Component, Fragment} from "react";
+import React, {Component, Fragment} from "react";
 import {connect} from "react-redux";
-import  * as actions  from "./actions";
+import * as actions from "./actions";
 import App from "../charts/FirstChart";
 
 
@@ -8,19 +8,21 @@ class Home extends Component {
     componentDidMount() {
         this.props.getUser();
     }
+
     render() {
-        return  <div>                     
-                <App />
-        {
-            
-            this.props.usersData!=null&&
-            this.props.usersData.result!=null&&
-            this.props.usersData.result.filter((data)=>data.id<=10).map((item)=>{
-              return (
-                 <li> {item.id}---{item.title}</li> )
-            })
-        }
-        </div>  
+        return <div>
+            <h1>test</h1>
+            <App/>
+            {
+
+                this.props.usersData != null &&
+                this.props.usersData.result != null &&
+                this.props.usersData.result.filter((data) => data.id <= 10).map((item) => {
+                    return (
+                        <li> {item.id}---{item.title}</li>)
+                })
+            }
+        </div>
 
     }
 }
@@ -28,8 +30,6 @@ class Home extends Component {
 const mapStateToProps = (state) => ({
     usersData: state.users
 });
-
-
 
 
 export default connect(mapStateToProps, actions)(Home);
